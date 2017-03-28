f4a6af98396df4c9d215a4597761495c14793033
Mustafa <yazarblog@gmail.com>
2017-03-29 02:37:41 +0300

    Opengl setup for multiple fragment shader outputs

--- samples/e2e_gl/framebuffer.cpp
+++ samples/e2e_gl/framebuffer.cpp
@@ -22,12 +22,22 @@ namespace e2e
 		glDeleteFramebuffers(1, &m_framebuffer_id);
 	}
 
-	void Framebuffer::renderToTexture(const Texture & rtt_tex)
+	void Framebuffer::renderToTexture(const Texture& rtt_tex)
 	{
 		glViewport(0, 0, rtt_tex.get_width(), rtt_tex.get_height());
 		glBindFramebuffer(GL_FRAMEBUFFER, m_framebuffer_id);
 		glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, rtt_tex.get_texture_id(), 0);
 	}
+
+    void Framebuffer::renderToTexture2D(const Texture& rtt_tex1, const Texture& rtt_tex2)
+    {
+        glViewport(0, 0, rtt_tex1.get_width(), rtt_tex1.get_height());
+        glBindFramebuffer(GL_FRAMEBUFFER, m_framebuffer_id);
+        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, rtt_tex1.get_texture_id(), 0);
+        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, GL_TEXTURE_2D, rtt_tex2.get_texture_id(), 0);
+        GLenum drawbuf[] = { GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1 };
+        glDrawBuffers(2, drawbuf);
+    }
 
 	void Framebuffer::renderToTextureLayer(const Texture & rtt_tex, int layer)
 	{
